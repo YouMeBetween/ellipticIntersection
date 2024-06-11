99ccadb90059a093bf231dde709393ff8407a75d
YouMeBetween <930798500@qq.com>
2024-06-11 14:56:46 +0800

    将解一元二次方程的过程写进一个函数中

--- ellipticIntersection.cpp
+++ ellipticIntersection.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 vector<pair<double, double>> calcExtremePoint(double, double, double, double, double, double);
+vector<double> solveQuadraticEquation(double, double, double);
 
 int main()
 {
@@ -19,25 +20,35 @@ int main()
 
 vector<pair<double, double>> calcExtremePoint(double A, double B, double C, double D, double E, double F)
 {
-    double a = 4 * A * A * B - A * C * C;
-    double b = 4 * A * B * D - 2 * A * C * E;
-    double c = B * D * D - C * D * E + C * C * F;
-    double delta = b * b - 4 * a * c;
+    vector<double> solution = solveQuadraticEquation(4 * A * A * B - A * C * C, 4 * A * B * D - 2 * A * C * E, B * D * D - C * D * E + C * C * F);
     pair<double, double> result;
     vector<pair<double, double>> extreme_points;
-    if (delta > 1e-6) {
-        result.first = (-b + sqrt(delta)) / (2 * a);
+    if (solution.size() == 2) {
+        result.first = solution.at(0);
         result.second = -(2 * A * result.first + D) / C;
         extreme_points.push_back(result);
-        result.first = (-b - sqrt(delta)) / (2 * a);
+        result.first = solution.at(1);
         result.second = -(2 * A * result.first + D) / C;
         extreme_points.push_back(result);
-    } else if (abs(delta) <= 1e-6) {
-        result.first = -b / (2 * a);
+    } else if (solution.size() == 1) {
+        result.first = solution.at(0);
         result.second = sqrt((-4 * A * B * result.first * result.first - 4 * B * D * result.first - 4 * B * F + E * E) / (4 * B * B)) - E / (2 * B);
         extreme_points.push_back(result);
         result.second = -sqrt((-4 * A * B * result.first * result.first - 4 * B * D * result.first - 4 * B * F + E * E) / (4 * B * B)) - E / (2 * B);
         extreme_points.push_back(result);
     }
     return extreme_points;
+}
+
+vector<double> solveQuadraticEquation(double a, double b, double c)
+{
+    vector<double> result;
+    double delta = b * b - 4 * a * c;
+    if (delta > 1e-6) {
+        result.push_back((-b + sqrt(delta)) / (2 * a));
+        result.push_back((-b - sqrt(delta)) / (2 * a));
+    } else if (abs(delta) <= 1e-6) {
+        result.push_back(-b / (2 * a));
+    }
+    return result;
 }
