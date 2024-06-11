0a2d045130621537e04fec0198ad004f2a5767ff
YouMeBetween <930798500@qq.com>
2024-06-11 09:50:23 +0800

    将极值点的计算放入一个函数中

--- ellipticIntersection.cpp
+++ ellipticIntersection.cpp
@@ -1,38 +1,41 @@
 #include <iostream>
 #include <ostream>
+#include <utility>
 #include <vector>
 #include <cmath>
 #include <cstdlib>
 using namespace std;
 
+vector<pair<double, double>>calcExtremePoint(double A, double B, double C, double D, double E, double F)
+{
+    double a = 4 * A * A * B - A * C * C;
+    double b = 4 * A * B * D - 2 * A * C * E;
+    double c = B * D * D - C * D * E + C * C * F;
+    double delta = b * b - 4 * a * c;
+    pair<double, double> result;
+    vector<pair<double, double>> extreme_points;
+    if (delta > 1e-6) {
+        result.first = (-b + sqrt(delta)) / (2 * a);
+        result.second = -(2 * A * result.first + D) / C;
+        extreme_points.push_back(result);
+        result.first = (-b - sqrt(delta)) / (2 * a);
+        result.second = -(2 * A * result.first + D) / C;
+        extreme_points.push_back(result);
+    } else if (abs(delta) <= 1e-6) {
+        result.first = -b / (2 * a);
+        result.second = sqrt((-4 * A * B * result.first * result.first - 4 * B * D * result.first - 4 * B * F + E * E) / (4 * B * B)) - E / (2 * B);
+        extreme_points.push_back(result);
+        result.second = -sqrt((-4 * A * B * result.first * result.first - 4 * B * D * result.first - 4 * B * F + E * E) / (4 * B * B)) - E / (2 * B);
+        extreme_points.push_back(result);
+    }
+    return extreme_points;
+}
+
 int main()
 {
-    double a1 = 3.0, b1 = 2.0, c1 = 4.8, d1 = 5.0, e1 = 1.0, f1 = -4.0;
-    double A = 4 * pow(a1, 2) * b1 - a1 * pow(c1, 2);
-    double B = 4 * a1 * b1 * d1 - 2 * a1 * c1 * e1;
-    double C = b1 * pow(d1, 2) - c1 * d1 * e1 + pow(c1, 2) * f1;
-    double delta = pow(B, 2) - 4 * A * C;
-    vector<double> x1, y1;
-    if (delta > 0) {
-        x1.push_back((-B + sqrt(delta)) / (2 * A));
-        x1.push_back((-B - sqrt(delta)) / (2 * A));
-    } else if (delta == 0) {
-        x1.push_back(-B / (2 * A));
-    }
-    for (auto s : x1) {
-        if (x1.size() > 1) {
-            y1.push_back(-(2 * a1 * s + d1) / c1);
-        } else {
-            y1.push_back(sqrt((-4 * a1 * b1 * pow(s, 2) - 4 * b1 * d1 * s - 4 * b1 * f1 + pow(e1, 2)) / (4 * pow(b1, 2))) - e1 / (2 * b1));
-            y1.push_back(-sqrt((-4 * a1 * b1 * pow(s, 2) - 4 * b1 * d1 * s - 4 * b1 * f1 + pow(e1, 2)) / (4 * pow(b1, 2))) - e1 / (2 * b1));
-        }
-    }
-    for (int i = 0; i != y1.size(); i++) {
-        if (x1.size() > 1) {
-            cout << "(" << x1.at(i) << ", " << y1.at(i) << ")" << endl;
-        } else {
-            cout << "(" << x1.at(0) << ", " << y1.at(i) << ")" << endl;
-        }
+    vector<pair<double, double>> extreme_points0 = calcExtremePoint(3.0, 2.0, 0.0, 5.0, 1.0, -4.0);
+    for (auto s : extreme_points0) {
+        cout << ".(" << s.first << ", " << s.second << ")" << endl;
     }
     system("pause");
 }
